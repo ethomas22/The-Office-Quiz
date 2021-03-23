27ee7c083d8e2543c0d9b3db9aacab1378f57fe7
Emily Thomas <ethomas@quint.co.uk>
2021-03-22 18:44:11 -0700

    added functions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,69 +1,104 @@
 // var with questions and answers
 var questions = [
-    {
-        question: "Who started the fire?",
-        answers: {
-            a: "Ryan",
-            b: "Phyllis",
-            c: "Michael",
-            d: "Pam"
-                },
-        correctAnswer: "a"
-     },
+  {
+    question: "Who started the fire?",
+    answers: {
+      a: "Ryan",
+      b: "Phyllis",
+      c: "Michael",
+      d: "Pam",
+    },
+    correctAnswer: "a",
+  },
 
-     {
-        question: "Who did Kevin get for secret santa?",
-        answers: {
-            a: "Kelly",
-            b: "Toby",
-            c: "Himself",
-            d: "Dwight"
-                },
-        correctAnswer: "c"
-     },
+  {
+    question: "Who did Kevin get for secret santa?",
+    answers: {
+      a: "Kelly",
+      b: "Toby",
+      c: "Himself",
+      d: "Dwight",
+    },
+    correctAnswer: "c",
+  },
 
-     {
-        question: "What does Stanley do during meetings?",
-        answers: {
-            a: "Sleep",
-            b: "Crossword Puzzles",
-            c: "Listen",
-            d: "Sleep"
-                },
-        correctAnswer: "b"
-     },
+  {
+    question: "What does Stanley do during meetings?",
+    answers: {
+      a: "Sleep",
+      b: "Crossword Puzzles",
+      c: "Listen",
+      d: "Sleep",
+    },
+    correctAnswer: "b",
+  },
 
-     {
-        question: "What is dwights favorite TV show?",
-        answers: {
-            a: "Battlestar Galactica",
-            b: "The Apprentice",
-            c: "Wife Swap",
-            d: "American Idol"
-                },
-        correctAnswer: "a"
-     },
+  {
+    question: "What is dwights favorite TV show?",
+    answers: {
+      a: "Battlestar Galactica",
+      b: "The Apprentice",
+      c: "Wife Swap",
+      d: "American Idol",
+    },
+    correctAnswer: "a",
+  },
 
-     {
-        question: "What did Michael burn is foot on?",
-        answers: {
-            a: "Fire Pit",
-            b: "Iron",
-            c: "BBQ",
-            d: "George Foreman Grill"
-                },
-        correctAnswer: "d"
-     },
-    
+  {
+    question: "What did Michael burn his foot on?",
+    answers: {
+      a: "Fire Pit",
+      b: "Iron",
+      c: "BBQ",
+      d: "George Foreman Grill",
+    },
+    correctAnswer: "d",
+  },
 ];
 
-document.querySelector("#quizQuestions").innerText=questions[0].question;
+var timeLeft = 60;
+var currentQuestion = -1;
 
-document.querySelector("#answerA").innerText=questions[0].answers.a;
-document.querySelector("#answerB").innerText=questions[0].answers.b;
-document.querySelector("#answerC").innerText=questions[0].answers.c;
-document.querySelector("#answerD").innerText=questions[0].answers.d;
+document.getElementById("startTimer").addEventListener("click", function () {
+  document.getElementById("timerText").innerText = timeLeft;
 
-document.getElementById("answerA").addEventListener("click", function(){
-    console.log("hello");
+  //   document.getElementById("main").style.display = "block";
+
+  //   document.getElementById("start").style.display = "none";
+
+  //   document.getElementById("start2").style.display = "block";
+  clockTimer();
+  nextQuestion();
+
+  function nextQuestion() {
+    currentQuestion++;
+    document.querySelector("#quizQuestions").innerText =
+      questions[currentQuestion].question;
+    document.querySelector("#answerA").innerText =
+      questions[currentQuestion].answers.a;
+    document.querySelector("#answerB").innerText =
+      questions[currentQuestion].answers.b;
+    document.querySelector("#answerC").innerText =
+      questions[currentQuestion].answers.c;
+    document.querySelector("#answerD").innerText =
+      questions[currentQuestion].answers.d;
+    document
+      .querySelector(".quizAnswer")
+      .addEventListener("click", function (event) {
+        nextQuestion()
+      });
+  }
+
+  function clockTimer() {
+    var countdown = setInterval(function () {
+      timeLeft--;
+      document.getElementById("timerText").innerText = timeLeft;
+      if (timeLeft <= 0 || currentQuestion == questions.length) {
+        // Quiz is done
+        clearInterval(countdown);
+        removeAnswers();
+        endingCard();
+      }
+    }, 1000);
+  }
 });
